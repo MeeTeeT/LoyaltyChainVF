c7afda32cefc8675e27213c921376134d5310345
MeeTeeT <thibaut.pauget@gmail.com>
2023-07-17 17:11:45 +0200

    Update Marketplace.js

--- frontend/src/pages/Marketplace/Marketplace.js
+++ frontend/src/pages/Marketplace/Marketplace.js
@@ -21,36 +21,7 @@ export default function Marketplace() {
     contractLTYMarketplace,
   } = useContext(WalletContext);
 
-  const sampleData = [
-    {
-      name: "NFT#1",
-      description: "Brand in creation...",
-      website: "http://axieinfinity.io",
-      image: "../../assets/images/loadingCard.png",
-      price: "wainting...",
-      currentlySelling: "True",
-      address: "0xe81Bf5A757CB4f7F82a2F23b1e59bE45c33c5b13",
-    },
-    /*,
-        {
-            "name": "NFT#2",
-            "description": "Alchemy's Second NFT",
-            "website":"http://axieinfinity.io",
-            "image":"https://gateway.pinata.cloud/ipfs/QmdhoL9K8my2vi3fej97foiqGmJ389SMs55oC5EdkrxF2M",
-            "price":"0.03ETH",
-            "currentlySelling":"True",
-            "address":"0xe81Bf5A757C4f7F82a2F23b1e59bE45c33c5b13",
-        },
-        {
-            "name": "NFT#3",
-            "description": "Alchemy's Third NFT",
-            "website":"http://axieinfinity.io",
-            "image":"https://gateway.pinata.cloud/ipfs/QmTsRJX7r5gyubjkdmzFrKQhHv74p5wT9LdeF1m3RTqrE5",
-            "price":"0.03ETH",
-            "currentlySelling":"True",
-            "address":"0xe81Bf5A757C4f7F82a2F23b1e59bE45c33c5b13",
-        },*/
-  ];
+  const sampleData = [];
   const [data, updateData] = useState(sampleData);
   const [dataFetched, updateFetched] = useState(false);
   const [viewNfts, setViewNfts] = useState(false);
